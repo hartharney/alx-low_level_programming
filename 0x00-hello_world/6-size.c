d46ac559ae6c7b8a0b5c66e4a40951c77e18f8f3
hartharney <hartharney@gmail.com>
2023-03-22 09:03:37 -0700

    rewrote 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
-*  You should produce the exact same output as in th example
-*  Warnings are allowed
-*  Your program should return: 0
-*/
+ * main - print the size of various types
+ *
+ * Description: use sizeof function to print sizes
+ * Return: 0
+ */
 int main(void)
 {
 	char c;
@@ -12,10 +13,11 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
-	printf("size of a char: %lu byte(s)\n", sizeof(c));
-	printf("size of an int: %lu byte(s)\n", sizeof(i));
-	printf("size of a long: %lu byte(s)\n", sizeof(li));
-	printf("size of a long long %lu byte(s)\n", sizeof(lli));
-	printf("size of a float: %lu byte(s)\n", sizeof(f));
+	
+	printf("size of a char: %zu bytes(s)\n", sizeof(c));
+	printf("size of an int: %zu bytes(s)\n", sizeof(i));
+	printf("size of a long int: %zu bytes(s)\n", sizeof(li));
+	printf("size of a long long int: %zu bytes(s)\n", sizeof(lli));
+	printf("size of a float: %zu bytes(s)\n", sizeof(f));
 	return (0);
 }
