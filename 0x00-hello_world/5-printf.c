f87067d5ae9bbfc4314ed9ac58c338aa10bda67c
hartharney <hartharney@gmail.com>
2023-03-22 08:39:06 -0700

    still betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /** 
- * main - A test c file using printf function
- *
- * Description: Using the main function
- * this program prints with proper grammar, but the outcome is a piece of art,
- * Return : 0
- */
+* main - print the string in the print function
+*
+* Description: Using the main function
+* this program prints with proper grammar, but the outcome is a piece of art,
+* Return : 0
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
