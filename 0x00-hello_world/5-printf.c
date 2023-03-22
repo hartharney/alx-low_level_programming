2b6d692dd0869497875922e28b42e360c80648ab
hartharney <hartharney@gmail.com>
2023-03-22 10:49:51 -0700

    final

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/** 
+/**
 * main - print the string in the print function
 *
 * Description: Using the main function
-* this program prints with proper grammar, but the outcome is a piece of art,
+* this program prints "with proper grammar, but the outcome is a piece of art,"
 * Return : 0
 */
 int main(void)
