55873140a83900c0a906237b18451f4ca88cccfb
hartharney <hartharney@gmail.com>
2023-03-24 08:37:08 -0700

    put proper return decription according to betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the print function
+* main - print the string in the put function
 *
 * Description: Using the main function
 * this program prints "with proper grammar, but the outcome is a piece of art,"
-* Return : 0
+* Return: Always 0 (Success)
 */
 int main(void)
 {
