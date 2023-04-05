a4ec9bc162ec6236fa955dd80adbb7f0213bed64
hartharney <hartharney@gmail.com>
2023-04-05 12:25:07 -0700

    print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _puts_recursion - this function prints a string using putchar
@@ -9,9 +8,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int len = strlen(s) + 1;
-
-	if(len == 0)
-		return 0;
-	_putchar(s) + _puts_recursion(s+1); 
+	if(*s)
+	{
+		_putchar(s);
+		_puts_recursion(s+1);
+	}
+	else
+		_putchar('\n');
 }
