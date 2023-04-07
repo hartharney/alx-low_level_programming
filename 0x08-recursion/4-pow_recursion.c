54cc5d495717154fd4a12e857d01209b8607d728
hartharney <hartharney@gmail.com>
2023-04-06 19:32:23 -0700

    fix script 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,16 +9,24 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int result = 1;
-
-	if (y < 0)
+	if (x == 0)
 	{
-		return (-1);
+		return (0);
 	}
-	if (y != 0)
+	if ( x != 0)
 	{
-		result *= x;
-		return (result + _pow_recursion(x, --y));
+		if (y < 0)
+		{
+			return (-1);
+		}
+		if (y == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (x * _pow_recursion(x, y - 1));
+		}
 	}
 	else
 	{
