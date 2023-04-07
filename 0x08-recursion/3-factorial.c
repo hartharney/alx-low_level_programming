e19e27308f9802bb3a9e2c5470c92c2b813099ed
hartharney <hartharney@gmail.com>
2023-04-06 18:00:54 -0700

    calculate factorial of n

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+i#include "main.h"
+
+/**
+ * factorial - function that calculates the factorial
+ * of a given number
+ * @n - factorial number passed as parameter
+ *
+ * Return: Always 0 (success);
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
