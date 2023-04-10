adf39b3f2911eff59d33c9f978c4d8079d2cb699
hartharney <hartharney@gmail.com>
2023-04-10 07:21:41 -0700

    fix cases where number arguments contain char

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,24 +11,24 @@
  */
 int __attribute__((unused)) main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
-		if (!(*argv[i] >= 48 && *argv[i] <= 57))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		if (argc == 1)
-		{
-			printf("0\n");
-			return (0);
-		}
-		else
-		{
-			sum += atoi(argv[i]);
-		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
