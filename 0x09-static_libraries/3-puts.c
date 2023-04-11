1127d996531e861f719cbe891b2860cf312717e6
hartharney <hartharney@gmail.com>
2023-04-11 08:47:59 -0700

    create static library, archive file

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - prints a text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
