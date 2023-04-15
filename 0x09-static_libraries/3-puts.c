36fde45e5f027b85d0599340dc907410b76274c3
hartharney <hartharney@gmail.com>
2023-04-15 03:53:52 -0700

    fix functions

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,18 @@
 #include "main.h"
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * _puts - prints to standard io
+ * @str: string character
  *
- * Return: Always 0 (Success)
+ * Return: string
  */
-int main(void)
+void _puts(const char* str) 
 {
-	void _puts(char *str);
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
 }
